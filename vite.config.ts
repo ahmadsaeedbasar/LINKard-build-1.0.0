fccf8c8ff672983746be6d131d38e678e5495dc9
ahmadsaeedbasar <ahmadsaeedbasar@users.noreply.github.com>
2026-01-12 13:05:52 +0500

    commit without dyad branding

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { fileURLToPath, URL } from 'node:url';
@@ -9,7 +8,7 @@ export default defineConfig(() => ({
     host: "::",
     port: 8080,
   },
-  plugins: [dyadComponentTagger(), react()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL('./src', import.meta.url)),
